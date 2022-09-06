8d17585f60d757fdbaad820da6e27d4874585f19
NoobSaiyan <ms19961998@gmail.com>
2022-09-06 12:56:59 +0530

    reading and wrting memory

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,9 +1,17 @@
 // The entry file of your WebAssembly module.
 
-export function add(a: i32, b: i32): i32 {
-  return a + b;
+memory.grow(2)
+store<u8>(0, 21)
+store<u8>(1, 69)
+
+export function readMemory(n: i32): i32 {
+  return load<u8>(n)
 }
 
-export function minusone(n: i32): i32{
+export function add(a: i32, b: i32): i32 {
+  return a + b
+}
+
+export function minusone(n: i32): i32 {
   return n - 1
 }
